e6a15a6cd53116b54eba1b503b57baad09766fb0
Taylor Li <t4ylor.li@gmail.com>
2026-04-14 12:06:35 -0400

    load auth0 config from env

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,20 @@
 const config = require("newsroom-core/webpack.config");
+const dotenv = require("dotenv");
+const webpack = require("webpack");
 
 config.entry.home_js = [
   config.entry.home_js,
   "./assets/auth/pr-manager-sso.ts",
 ];
 config.entry.firebase_login_js = "./assets/auth/firebase/login.ts";
+
+require("dotenv").config();
+
+config.plugins = [
+  ...(config.plugins || []),
+  new webpack.DefinePlugin({
+    "process.env": JSON.stringify(dotenv.config().parsed),
+  }),
+];
 
 module.exports = config;
