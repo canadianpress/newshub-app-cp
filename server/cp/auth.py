336bc982b0aa4418641dd1473f8cdffddcf5e996
Taylor Li <t4ylor.li@gmail.com>
2026-05-29 13:30:43 -0400

    remove unused fallback

--- server/cp/auth.py
+++ server/cp/auth.py
@@ -259,7 +259,7 @@ async def oidc_token(args, params, request: Request):
     claims = _build_oidc_claims(
         request,
         session_data,
-        audience=code_data.get("client_id") or client_id or "auth0",
+        audience=code_data.get("client_id") or client_id,
         nonce=code_data.get("nonce"),
     )
     access_token = str(uuid4())
