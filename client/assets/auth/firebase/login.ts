b204c38b01ad304ae209f31f43e76a567ef67690
Taylor Li <t4ylor.li@gmail.com>
2026-05-29 11:43:27 -0400

    add guard, generic query selectors

--- client/assets/auth/firebase/login.ts
+++ client/assets/auth/firebase/login.ts
@@ -1,10 +1,17 @@
 import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth } from "newsroom-core/assets/auth/firebase/init";
 
-const form = document.getElementById("formLogin") as HTMLFormElement;
-const firebaseStatus = document.getElementById(
-  "firebase-status",
-) as HTMLInputElement;
+const form = document.querySelector<HTMLFormElement>("#formLogin");
+if (!form) {
+  throw new Error("#formLogin not found");
+}
+
+const firebaseStatus =
+  document.querySelector<HTMLInputElement>("#firebase-status");
+if (!firebaseStatus) {
+  throw new Error("#firebase-status not found");
+}
+
 const params = new URLSearchParams(window.location.search);
 if (params.get("email")) {
   form["email"].value = params.get("email");
@@ -15,28 +22,27 @@ const sendTokenToServer = (token: string) => {
 };
 
 auth.onAuthStateChanged((user) => {
-  if (user != null) {
-    if (params.get("user_error") === "1") {
-      return;
-    }
-
-    if (params.get("logout") === "1") {
-      signOut(auth);
-      return;
-    }
-
-    const tokenError = params.get("token_error");
-
-    user.getIdToken(tokenError === "1").then(sendTokenToServer);
+  if (user === null) return;
+  if (params.get("user_error") === "1") return;
+  if (params.get("logout") === "1") {
+    signOut(auth);
+    return;
   }
+
+  const tokenError = params.get("token_error");
+  user.getIdToken(tokenError === "1").then(sendTokenToServer);
 });
 
 form.onsubmit = (event) => {
   event.preventDefault();
 
   const data = new FormData(form);
-  const email = data.get("email") as string;
-  const password = data.get("password") as string;
+
+  const email = data.get("email");
+  if (typeof email !== "string") return;
+
+  const password = data.get("password");
+  if (typeof password !== "string") return;
 
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => userCredential.user.getIdToken())
